85082950214124c6a079f55f2888bfe613de1395
Daniel Bustillos <bustillosdaniel4@gmail.com>
2022-11-05 21:15:40 -0300

    created description component

---
+++ src/components/ProductDetails/Description/Description.jsx
@@ -0,0 +1,67 @@
+import "./Description.css";
+export default function Description() {
+  return (
+    <div className="description">
+      <h3>Descripción</h3>
+      <p>
+        SOMOS MULTIVENTAS AZ <br />
+        MERCADOLIBRE PLATINUM UNOS DE LOS MEJORES VENDEDORES EN EL SITIO!!!{" "}
+        <br />
+        <br />
+        Te presentamos: <br />
+        PELUCHE STITCH AZUL Y ROSA <br />
+        PRECIO POR UNIDAD <br />
+        <br />
+        TAMBIÉN TENEMOS MÁS TAMAÑOS <br />
+        <br />
+        LLÉVATE AL MEJOR PRECIO <br />
+        <br />
+        Excelente calidad y diseño Premiun <br />
+        Ojos bordados y relleno de vellón súper soft <br />
+        Muy suave y más hermoso que nunca!! <br />
+        <br />
+        Descripción: 20cm de alto aprox <br />
+        <br />
+        HACEMOS PRECIO POR MAYOR <br />
+        ( EL MEJOR PRECIO DEL MERCADO) <br />
+        <br />
+        FOTOS REALES DEL PRODUCTO <br />
+        <br />
+        EL PRECIO ES POR UNIDAD <br />
+        <br />
+        Hacemos descuentoPORMAYOR… <br />
+        <br />
+        Estamos Ubicados: <br />
+        -En Balvanera (MicroCentro) <br />
+        a 6 cuadras de las Av.CALLAO Y Av.CORRIENTES <br />
+        A 5 cuadras de Puerrydon y Rivadavia <br />
+        <br />
+        Horarios de Atención: <br />
+        -Lunes a Viernes: 10hs a 18hs <br />
+        -Sábados: 10 a 14hs
+        <br />
+        <br />
+        MÉTODOS DE ENVIOS: <br />
+        <br />
+        -HACEMOS ENVIOS A TODO EL PAIS MEDIANTE MERCADOENVIOS (SE ENVÍA EL MISMO
+        DÍA O AL SIGUIENTE SEGÚN EL HORARIO DE COMPRA) <br />
+        <br />
+        -HACEMOS ENVÍOS AL DÍA MEDIANTE ENVIOS FLEX TE LLEGA EL MISMO DÍA SI
+        REALIZAS LA COMPRA ANTES DE LAS 15hs (el tiempo de demora es según el
+        cadete que te puede llegar entre las 17 a 22hs) <br />
+        <br />
+        IMPORTANTE: Debajo de cada publicación puede ver los productos que
+        tenemos publicados en "Más publicaciones del Vendedor", si usted va a
+        realizar la compra con Mercadoenvíos por más de un producto y quiere
+        abonar un solo envío puede seleccionar la opción de agregar en el
+        carrito. <br />
+        <br />
+        Tu consulta no molesta, preguntar ante cualquier duda. Recorda que tu
+        oferta es un compromiso de compra y deberás comunicarte dentro de los
+        2-3 dias luego de la misma. <br />
+        <br />
+        Visita mis otros artículos publicados. DESDE YA GRACIAS
+      </p>
+    </div>
+  );
+}
--- src/components/ProductDetails/ProductMainContent/ProductMainContent.jsx
+++ src/components/ProductDetails/ProductMainContent/ProductMainContent.jsx
@@ -1,12 +1,14 @@
 import "./ProductMainContent.css";
 import OpinionSection from "../OpinionSection/OpinionSection";
 import QuestionsAnswers from "../QuestionsAnswers/QuestionsAnswers";
+import Description from "../Description/Description";
 
 export default function ProductMainContent() {
   return (
     <div className="wrapper-main-content">
       <div className="product-main-content">
         <div className="left">
+          <Description />
           <QuestionsAnswers />
           <OpinionSection />
         </div>
